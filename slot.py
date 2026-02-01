09ccb2246ee84baa40430827a69cecf270821da2
JoonHak Kim <jk7296@nyu.edu>
2026-02-01 14:23:24 +0400

    created bigger slot definitions

--- slot.py
+++ slot.py
@@ -2,6 +2,7 @@
 import ctypes
 from enum import IntEnum
 
+#Maybe loading from .json file might be nicer.
 class ProcTaskFnID(IntEnum):
     TERMINATE     = 0x0000
     READ_FILE     = 0x1000
@@ -26,31 +27,82 @@ def register_fn_id(name: str, value: int) -> int:
 
 class TaskSlot128(ctypes.Structure):
     '''
+    This is a generic 128-byte task slot to hold task_id, function_id, arguments and meta data.
+    For more flexible usage, refers to TaskSlot196 or 256. 
+    tsk_id: tsk_id is an increment counter distributed to each task.
+    fn_id: fn_id is a mapping to signify which function worker has to process.
+    args: stores arguments for a fuction that is mapped to fn_id. 
+    '''
+    _align_ = 128 #make sure its a multiple of 64 bytes.
+    _fields_ = [
+        ("tsk_id", ctypes.c_uint32),           # 4 bytes
+        ("fn_id", ctypes.c_uint32),          # 4 bytes
+        ("args", ctypes.c_int64 * 10),        # 80 bytes generic args
+        ("meta", ctypes.c_uint8 * 40),        # 40 bytes meta data
+    ]
+
+
+
+class TaskSlot196(ctypes.Structure):
+    '''
     This is a generic 192-byte task slot to hold task_id, function_id, arguments and meta data.
     For more flexible usage, refers to TaskSlot256 or TaskSlot512
 
     tsk_id: tsk_id is an increment counter distributed to each task.
     fn_id: fn_id is a mapping to signify which function worker has to process.
-    
     args: stores arguments for a fuction that is mapped to fn_id. 
-    IT MUST NOT TAKE ARGUMENTS FROM ANY DECORATOR FUNCTIONS!!
-    It is often recommended to create a function that is already decorated and create a mapping to fn_id.
-    Refer to register_fn_id()
-
-    
-    #META DATA SECTION
-    (log: if set to 1, the work will be logged)
-    (ts: if set to 1, the log will contain time stamp. It will be ignore if log = 0)
-    ()
-    meta: meta data holds args about:
-        d
     '''
-    _align_ = 64
+    _align_ = 196 #make sure its a multiple of 64 bytes.
     _fields_ = [
         ("tsk_id", ctypes.c_uint32),           # 4 bytes
         ("fn_id", ctypes.c_uint32),          # 4 bytes
-        ("args", ctypes.c_uint64 * 2),        # 16 bytes generic args
-        ("meta", ctypes.c_uint8 * 40),        # 40 bytes auxiliary data
+        ("args", ctypes.c_int64 * 15),        # 120 bytes generic args
+        ("meta", ctypes.c_uint8 * 64),        # 64 bytes meta data
+    ]
+    
+class TaskSlot128_cargs(ctypes.Structure):
+    '''
+    This char+int arugment 128-byte task slot to hold task_id, function_id, arguments and meta data.
+    For more flexible usage, refers to TaskSlot256 or TaskSlot512
+
+    tsk_id: tsk_id is an increment counter distributed to each task.
+    fn_id: fn_id is a mapping to signify which function worker has to process.
+    args: stores arguments for a fuction that is mapped to fn_id. 
+    c_args: stores argument in char type. Use '\0' as a termination condition. 
+        and '0x20' or white space to parse arguments.
+
+        d
+    '''
+    _align_ = 128 #make sure its a multiple of 64 bytes.
+    _fields_ = [
+        ("tsk_id", ctypes.c_uint32),          # 4 bytes
+        ("fn_id", ctypes.c_uint32),           # 4 bytes
+        ("args", ctypes.c_int64 * 2),        # 16 bytes generic args
+        ("c_args", ctypes.c_char * 64),       # 64 bytes char args
+        ("meta", ctypes.c_uint8 * 40),        # 40 bytes meta data
     ]
 
 
+
+class TaskSlot196_cargs(ctypes.Structure):
+    '''
+    This char+int arugment 196-byte task slot to hold task_id, function_id, arguments and meta data.
+    For more flexible usage, refers to TaskSlot256 or TaskSlot512
+
+    tsk_id: tsk_id is an increment counter distributed to each task.
+    fn_id: fn_id is a mapping to signify which function worker has to process.
+    args: stores arguments for a fuction that is mapped to fn_id. 
+    c_args: stores argument in char type. Use '\0' as a termination condition. 
+            and '0x20' or white space to parse arguments.
+
+    '''
+    _align_ = 196 #make sure its a multiple of 64 bytes.
+    _fields_ = [
+        ("tsk_id", ctypes.c_uint32),          # 4 bytes
+        ("fn_id", ctypes.c_uint32),           # 4 bytes
+        ("args", ctypes.c_int64 * 5),         # 40 bytes generic args
+        ("c_args", ctypes.c_char * 84),       # 84 bytes char args
+        ("meta", ctypes.c_uint8 * 64),        # 64 bytes meta data
+    ]
+
+    ####
